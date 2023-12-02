a2ad46dd975e3ffc44f2bd97dc0e0db4a88445c8
23357614 <jackcasey614@gmail.com>
2023-12-02 20:43:43 +0000

    added constructor and to string for Node, subject to change in future

--- src/main/java/network/Node.java
+++ src/main/java/network/Node.java
@@ -7,7 +7,23 @@ public class Node {
     Edge[] edges;
     entities.Entity[] occupants;
 
+    public Node(String id, float x, float y) {
+        this.x = (int) x;   // TODO: to float or not to float?
+        this.y = (int) y;
+
+        this.id = id;
+    }
+
     public static boolean equals(Node n, Node userNode) {   // added for redundancy
         return n.id.equals(userNode.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Node {" +
+                "id='" + id + '\'' + "}";
+//                ", (x=" + x +
+//                ", y=" + y +  ")" +
+//                '}';
     }
 }
