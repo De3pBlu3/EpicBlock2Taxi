648caa5fc001209a35f9f5ab5b89b39e4f0d599d
23357614 <jackcasey614@gmail.com>
2023-12-02 20:43:02 +0000

    added to string for edge class

--- src/main/java/network/Edge.java
+++ src/main/java/network/Edge.java
@@ -1,8 +1,23 @@
 package network;
 
+import java.util.Arrays;
+
 public class Edge {
     int weight; // length of edge / speed of edge = time to traverse edge (in ticks)
     Node start;
     Node end;
     entities.Entity[] occupants;
+
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "weight = " + weight +
+                ", start = " + start +
+                ", end = " + end +
+                ", occupants = " + Arrays.toString(occupants) +
+                '}';
+    }
 }
+
