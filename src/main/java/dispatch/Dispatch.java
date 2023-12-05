6eb0a2593defce4b97d1e903c1952c4b6507206e
De3pBlu3 <jackontheedge@gmail.com>
2023-12-05 13:34:18 +0000

    added docstrings to dispatch class

--- src/main/java/dispatch/Dispatch.java
+++ src/main/java/dispatch/Dispatch.java
@@ -27,14 +27,33 @@ public class Dispatch implements testing.VehicleHiringTest{
         return false;
     }
 
+    /**
+     *  Remove the vehicle with the specified reg number from the map if it is registered
+     *  and return true. If vehicle is not registered or is not on map the method returns false.
+     * @param reg
+     * @return
+     */
     public boolean testRemoveVehicle(String reg) {
         return false;
     }
 
+    /**
+     * Return the location of vehicle specified by the reg number if it is registered and added
+     * to the map, null otherwise.
+     * @param reg
+     * @return
+     */
     public Location testGetVehicleLoc(String reg) {
         return null;
     }
 
+    /**
+     * Return a list of all vehicles registration numbers located within a square of side 2*r
+     * centered at location loc (inclusive of the boundaries).
+     * @param loc
+     * @param r
+     * @return
+     */
     public List<String> testGetVehiclesInRange(Location loc, int r) {
         return null;
     }
