684594749b5025869fce899567dc36bba6935908
De3pBlu3 <jackontheedge@gmail.com>
2023-12-05 13:17:04 +0000

    Added location basic layout

--- src/main/java/not_implemented_yet/Location.java
+++ src/main/java/not_implemented_yet/Location.java
@@ -1,6 +1,12 @@
 package not_implemented_yet;
 
+import network.NetworkComponent;
+
 // Added this to get rid of those annoying
 // "cannot resolve symbol..." errors
 public class Location {
+    private float x;
+    private float y;
+    private NetworkComponent currentLocation;
+
 }
