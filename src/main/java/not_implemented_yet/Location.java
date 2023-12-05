9093487f513fa4374f822bb7db58a52388aeca27
De3pBlu3 <jackontheedge@gmail.com>
2023-12-05 13:22:16 +0000

    added getter for current location in net and coords

--- src/main/java/not_implemented_yet/Location.java
+++ src/main/java/not_implemented_yet/Location.java
@@ -1,5 +1,4 @@
 package not_implemented_yet;
-
 import network.NetworkComponent;
 
 // Added this to get rid of those annoying
@@ -7,6 +6,21 @@ import network.NetworkComponent;
 public class Location {
     private float x;
     private float y;
-    private NetworkComponent currentLocation;
+    private NetworkComponent currentNetLocation;
+
+
+    public Location(NetworkComponent currentNetLocation) {
+        this.currentNetLocation = currentNetLocation;
+//        this.x = currentNetLocation.getX(); // TODO implement getX() and getY() in NetworkComponent
+//        this.y = currentNetLocation.getY();
+    }
+
+    public NetworkComponent getCurrentNetLocation() {
+        return this.currentNetLocation;
+    }
+
+    public float[] getCoordinates() {
+        return new float[] {this.x, this.y};
+    }
 
 }
