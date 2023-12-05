3cafc0cf793f7aca7853f78dc6961240b194b772
De3pBlu3 <jackontheedge@gmail.com>
2023-12-05 14:37:00 +0000

    added adder and getter to network Component

--- src/main/java/network/NetworkComponent.java
+++ src/main/java/network/NetworkComponent.java
@@ -1,5 +1,30 @@
 package network;
 
 public class NetworkComponent {
-    entities.Entity[] occupants;
+    protected entities.Entity[] occupants;
+
+    public void addOccupant(entities.Entity occupant) {
+        // check if occupant is already in occupants
+        for (entities.Entity e : occupants) {
+            if (e == occupant) {
+                return;
+            }
+        }
+
+        // create new array with length + 1 TODO dynamic-ify
+        entities.Entity[] newOccupants = new entities.Entity[occupants.length + 1];
+
+        // copy
+        System.arraycopy(occupants, 0, newOccupants, 0, occupants.length);
+
+
+        // append
+        newOccupants[occupants.length] = occupant;
+        occupants = newOccupants;
+
+
+    }
+    public entities.Entity[] getOccupants(){
+        return occupants;
+    }
 }
