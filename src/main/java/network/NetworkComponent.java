b5baf85ac78522fba445c6e4b5c7f5cade86d1bc
De3pBlu3 <jackontheedge@gmail.com>
2023-12-06 21:17:11 +0000

    added abstract Edge getter in range to network component

--- src/main/java/network/NetworkComponent.java
+++ src/main/java/network/NetworkComponent.java
@@ -6,6 +6,17 @@ import lists.DynamicArray;
 public class NetworkComponent {
     DynamicArray<Entity> occupants = new DynamicArray<Entity>();
 
+    /**
+     * Returns all edges within a certain range of this node.
+     * If the weightLimit is 0, then nothing is returned.
+     * @param weightLimit how far down the network to go
+     * @return an array of edges within range
+     */
+    public Edge[] getEdgesInRange(int weightLimit) {
+        System.out.println("getEdgesInRange not implemented for generic and edge classes");
+        return null;
+    }
+
     public void addOccupant(entities.Entity occupant) {
         // check if occupant is already in occupants
         for (Entity e : occupants) {
