41ca6a13de3e8066d72833ea419800269ea6505f
De3pBlu3 <jackontheedge@gmail.com>
2023-12-11 17:13:18 +0000

    init dropoff, pickup, scheduler, java file

---
+++ src/main/java/time/Scheduler.java
@@ -0,0 +1,11 @@
+package time;
+
+public class Scheduler {
+    // will be used to schedule events based on weights
+    // (origin node) taxi --- 5 --- node 1 ---- 2 ---- node 2
+    // tick 0: create move event for taxi to node 1
+    // tick 5: create move event for taxi to node 2
+    // tick 7: taxi arrives at node 2
+
+
+}
