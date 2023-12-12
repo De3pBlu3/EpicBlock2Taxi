c23160db4ca931ddeb372177fbf0022fd5e34ca4
De3pBlu3 <jackontheedge@gmail.com>
2023-12-12 23:00:47 +0000

    scheduler class created

--- src/main/java/time/Scheduler.java
+++ src/main/java/time/Scheduler.java
@@ -1,4 +1,13 @@
+
 package time;
+
+import network.Network;
+import dispatch.Dispatch;
+import entities.Taxi;
+import network.Node;
+import network.Location;
+
+import java.util.Arrays;
 
 public class Scheduler {
     // will be used to schedule events based on weights
@@ -7,5 +16,13 @@ public class Scheduler {
     // tick 5: create move event for taxi to node 2
     // tick 7: taxi arrives at node 2
 
+    Timeline timeline;
+    Network network;
+    Dispatch dispatch;
+
+    public Scheduler(Timeline timeline, Network network, Dispatch dispatch) {
+        this.timeline = timeline;
+        this.network = network;
+        this.dispatch = dispatch;
 
 }
